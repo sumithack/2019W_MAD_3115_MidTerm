a544d0cd5726ca0f240c5e38c27d724505585193
MacStudent <macstudent@126-iMac03.local>
2019-03-20 18:39:38 -0400

    LoginScreen

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var rememberSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let userDefault = UserDefaults.standard
+        if let email = userDefault.string(forKey: "userEmail"){
+            
+        self.txtUsername.text = email
+            if let pwd = userDefault.string(forKey: "userPassword"){
+                self.txtPassword.text = pwd
+            
+            rememberSwitch.isOn = true
+            }
+            else{
+                rememberSwitch.isOn = false
+            }
+            
+        }
+       
     }
 
 
+    @IBAction func btnLoginClick(_ sender: Any) {
+        
+        let email = self.txtUsername.text
+        let pwd = self.txtPassword.text
+         let userDefault = UserDefaults.standard
+        
+        if(email == "admin" && pwd == "admin@123"){
+            print("Login Successfully")
+           
+            userDefault.set(email, forKey: "userEmail")
+            userDefault.set(pwd, forKey: "userPassword")
+        }
+        else{
+            print("login Unsuccessful")
+            let alert = UIAlertController.init(title: "Unsuccessfull", message: "Username Password Wrong", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Retype", style: UIAlertAction.Style.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+            userDefault.removeObject(forKey: "userEmail")
+            userDefault.removeObject(forKey: "userPassword")
+        }
+        
+    }
 }
 
